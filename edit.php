65c5a3f4696f06917fdd434859c5698c7b649749
HG-KinjoYuta <102797562+HG-KinjoYuta@users.noreply.github.com>
2022-04-07 13:07:42 +0900

    一時保存

---
+++ edit.php
@@ -0,0 +1,19 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>編集</title>
+</head>
+<body>
+    <h1>編集</h1>
+    <form action
+    <label></label><textarea></textarea>
+    
+</body>
+</html>
